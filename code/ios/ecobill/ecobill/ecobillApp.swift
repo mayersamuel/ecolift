969a208c713cd1627b5c8ed248c795f937b15709
linusX1402 <linus@wizany.at>
2025-02-10 11:26:28 +0100

    ADD[ios]: Setup

---
+++ code/ios/ecobill/ecobill/ecobillApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ecobillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ code/ios/ecobill/ecobillTests/ecobillTests.swift
@@ -0,0 +1,17 @@
+//
+//  ecobillTests.swift
+//  ecobillTests
+//
+//  Created by Linus Wizany on 10.02.25.
+//
+
+import Testing
+@testable import ecobill
+
+struct ecobillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
